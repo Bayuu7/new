ec2819acb4521b90fbd3bce758eafab959b33ccc
Bayuu7 <103884982+Bayuu7@users.noreply.github.com>
2025-08-10 19:43:52 +0700

    ForgotPassword.tsx

---
+++ App/Login/components/ForgotPassword.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function ForgotPassword() {
+  return (
+    <a href="/forgot-password" className="forgot-password-link">
+      Forgot Password?
+    </a>
+  );
+}
