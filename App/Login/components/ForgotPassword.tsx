aacacbedf0b4b428428e9f4bebbf3216a777adc2
Bayuu7 <103884982+Bayuu7@users.noreply.github.com>
2025-08-31 16:43:10 +0700

    Delete App/Login directory

--- App/Login/components/ForgotPassword.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function ForgotPassword() {
-  return (
-    <a href="/forgot-password" className="forgot-password-link">
-      Forgot Password?
-    </a>
-  );
-}
--- App/Login/components/RegisterLink.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function RegisterLink() {
-  return (
-    <a href="/register" className="register-link">
-      Register
-    </a>
-  );
-}
